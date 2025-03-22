67f979d132c072ddfb1a3e2ff151d88eaf72f4da
Phil Crump <phil@philcrump.co.uk>
2025-03-22 20:17:57 +0000

    firmware/src/config.rs: Remove embassy example code from file.

--- firmware/src/config.rs
+++ firmware/src/config.rs
@@ -1,4 +1,4 @@
-
+// This module is a work in progress
 
 use embassy_rp::flash::{Async, ERASE_SIZE, FLASH_BASE};
 
@@ -42,25 +42,4 @@ pub fn multiwrite_bytes(flash: &mut embassy_rp::flash::Flash<'_, FLASH, Async, F
 
     }
 
-    info!("Addr of flash block is {:x}", ADDR_OFFSET + FLASH_CONFIG_OFFSET + FLASH_BASE as u32);
-    info!("Contents start with {=[u8]}", read_buf[0..4]);
-
-    defmt::unwrap!(flash.blocking_erase(ADDR_OFFSET + FLASH_CONFIG_OFFSET, ADDR_OFFSET + FLASH_CONFIG_OFFSET + ERASE_SIZE as u32));
-
-    defmt::unwrap!(flash.blocking_read(ADDR_OFFSET + FLASH_CONFIG_OFFSET, &mut read_buf));
-    info!("Contents after erase starts with {=[u8]}", read_buf[0..4]);
-    if read_buf.iter().any(|x| *x != 0xFF) {
-        defmt::panic!("unexpected");
-    }
-
-    defmt::unwrap!(flash.blocking_write(ADDR_OFFSET + FLASH_CONFIG_OFFSET, &[0x01]));
-    defmt::unwrap!(flash.blocking_write(ADDR_OFFSET + FLASH_CONFIG_OFFSET + 1, &[0x02]));
-    defmt::unwrap!(flash.blocking_write(ADDR_OFFSET + FLASH_CONFIG_OFFSET + 2, &[0x03]));
-    defmt::unwrap!(flash.blocking_write(ADDR_OFFSET + FLASH_CONFIG_OFFSET + 3, &[0x04]));
-
-    defmt::unwrap!(flash.blocking_read(ADDR_OFFSET + FLASH_CONFIG_OFFSET, &mut read_buf));
-    info!("Contents after write starts with {=[u8]}", read_buf[0..4]);
-    if &read_buf[0..4] != &[0x01, 0x02, 0x03, 0x04] {
-        defmt::panic!("unexpected");
-    }
 }
